ce6a4ae58373f443a8016a557b2c1dc832366b94
Navya <navyakola104@gmail.com>
2024-09-30 13:08:15 +0530

    Add files via upload

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
+}
+
+output "alb_zone_id" {
+  value       = aws_lb.example.zone_id  # Replace 'example' with your Load Balancer resource name
+  description = "The zone ID of the load balancer"
+}
+
+output "domain_name" {
+  value       = "getterraform.com"
+  description = "The domain name for the application"
+}
