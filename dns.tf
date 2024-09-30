0bb33de0d170c29524b6b54e98d58498e700eb2f
Navya <navyakola104@gmail.com>
2024-09-30 14:36:51 +0530

    Update dns.tf

--- dns.tf
+++ dns.tf
@@ -1,23 +1,30 @@
+# Read the outputs from the existing configuration
+data "terraform_remote_state" "outputs" {
+  backend = "local"
+
+  config = {
+    path = "./terraform.tfstate"
+  }
+}
+
 # Fetch the Route 53 hosted zone for the domain
 data "aws_route53_zone" "primary" {
   name = data.terraform_remote_state.outputs.outputs.domain_name
 }
 
-# Check if the record already exists
-data "aws_route53_record" "existing" {
+# Check if the Route 53 record already exists
+data "aws_route53_record" "existing_record" {
   zone_id = data.aws_route53_zone.primary.zone_id
   name     = data.terraform_remote_state.outputs.outputs.domain_name
   type     = "A"
-
-  # Use count to conditionally create
-  count = length(aws_route53_record.existing) == 0 ? 1 : 0
 }
 
-# Create an A Record that points to the ALB DNS if it doesn't exist
+# Create an A Record that points to the ALB DNS, only if it doesn't already exist
 resource "aws_route53_record" "getterraform_com" {
-  count   = data.aws_route53_record.existing.count > 0 ? 0 : 1
+  count   = length(data.aws_route53_record.existing_record) == 0 ? 1 : 0
 
   zone_id = data.aws_route53_zone.primary.zone_id
+
   name    = data.terraform_remote_state.outputs.outputs.domain_name
   type    = "A"
 
