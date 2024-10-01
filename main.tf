c8bfefbffcec658836c39d72e73d73d33800f42e
Navya <navyakola104@gmail.com>
2024-10-01 12:39:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_launch_configuration" "example" {
 
     user_data = <<-EOF
                 #!/bin/bash
-                echo "Hai" > index.html
+                echo "Hai..............." > index.html
                 nohup busybox httpd -f -p ${var.server_port} &
                 echo "Web server started on port ${var.server_port}"
                 EOF
